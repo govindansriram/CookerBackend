22ee3a661fc7b3ed665a6f16e990616a9a7b9cb8
Aneesh Pothuru <pothuruaneesh23@gmail.com>
2023-05-10 23:49:41 -0700

    reviews added

--- resources/js/components/place/place.jsx
+++ resources/js/components/place/place.jsx
@@ -1,10 +1,79 @@
-import React from "react";
+import React from 'react';
+import Post from '../post/post';
+import { Form, Input, Button } from "antd";
+import { getPosts, createPost } from '../../api/postApi';
+import styled from "styled-components";
+
+const { TextArea } = Input;
+
+const StyledCard = styled.div`
+  background-color: #fff;
+  padding: 20px;
+  margin: 20px;
+`;
 
 const Place = () => {
+    const [posts, setPosts] = React.useState([]);
+    const [form] = Form.useForm();
+    const name = localStorage.getItem("name");
+  
+    const onFinish = async (values) => {
+      try {
+        const response = await createPost(
+          values.title,
+          name,
+          values.body,
+          'R',
+          values.image_link
+        );
+        console.log(response);
+        form.resetFields();
+      } catch (error) {
+        console.log(error);
+      }
+    };
+  
+    React.useEffect(() => {
+      const fetchPosts = async () => {
+        try {
+          const response = await getPosts();
+          setPosts(response);
+        } catch (error) {
+          console.log(error);
+        }
+      };
+  
+      fetchPosts();
+    }, []);
+  
     return (
-        <>
-            <h1>Restaurants</h1>
-        </>
+      <>
+        <h1>Restaurants</h1>
+        <h2>Post your review</h2>
+        <StyledCard>
+        <Form form={form} onFinish={onFinish}>
+          <Form.Item label="Title" name="title" rules={[{ required: true }]}>
+            <Input />
+          </Form.Item>
+          <Form.Item label="Body" name="body" rules={[{ required: true }]}>
+            <TextArea />
+          </Form.Item>
+          <Form.Item label="Image Link" name="image_link">
+            <Input />
+          </Form.Item>
+          <Form.Item>
+            <Button type="primary" htmlType="submit">
+              Create Post
+            </Button>
+          </Form.Item>
+        </Form>
+        </StyledCard>
+        {
+          posts.map((post) => 
+            post.type === 'R' && <Post key={post._id} {...post} />
+          )
+        }
+      </>
     );
 }
 
